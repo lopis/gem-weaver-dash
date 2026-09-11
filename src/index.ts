593d62c70285bdccfac8aedb54abc5f7191084c0
lopis <lopis@users.noreply.github.com>
2026-09-11 14:23:20 +0200

    remove favicon

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { loadingState } from './game-states/loading.state';
 import { emit } from './core/event';
 import { GAME_EVENT_PAUSE, GAME_EVENT_UNPAUSE } from './game/event-manifest';
 
-fav.href = 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 100 100\'%3E%3Ctext y=\'.9em\' font-size=\'85\'%3E🍓%3C/text%3E%3C/svg%3E';
+// fav.href = 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 100 100\'%3E%3Ctext y=\'.9em\' font-size=\'85\'%3E🍓%3C/text%3E%3C/svg%3E';
 
 let previousTime = 0;
 let paused = false;
